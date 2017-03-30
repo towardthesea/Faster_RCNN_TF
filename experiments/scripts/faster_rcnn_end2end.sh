fcb8d1c545bad7f41e42ee691dd7d907c15c31e9
Phuong Nguyen <ph17dn@gmail.com>
2017-03-30 15:33:18 +0200

    fix error of test in script and add demo with yarp

--- experiments/scripts/faster_rcnn_end2end.sh
+++ experiments/scripts/faster_rcnn_end2end.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Usage:
-# ./experiments/scripts/faster_rcnn_end2end.sh GPU NET DATASET [options args to {train,test}_net.py]
+# ./experiments/scripts/faster_rcnn_end2end.sh GPU GPU_ID NET DATASET [options args to {train,test}_net.py]
 # DATASET is either pascal_voc or coco.
 #
 # Example:
-# ./experiments/scripts/faster_rcnn_end2end.sh 0 VGG_CNN_M_1024 pascal_voc \
+# ./experiments/scripts/faster_rcnn_end2end.sh gpu 0 VGG_CNN_M_1024 pascal_voc \
 #   --set EXP_DIR foobar RNG_SEED 42 TRAIN.SCALES "[400, 500, 600, 700]"
 
 set -x
@@ -27,7 +27,7 @@ case $DATASET in
     TRAIN_IMDB="voc_2007_trainval"
     TEST_IMDB="voc_2007_test"
     PT_DIR="pascal_voc"
-    ITERS=70000
+    ITERS=80000
     ;;
   coco)
     # This is a very long and slow training schedule
@@ -44,7 +44,8 @@ case $DATASET in
     ;;
 esac
 
-LOG="experiments/logs/faster_rcnn_end2end_${NET}_${EXTRA_ARGS_SLUG}.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
+#LOG="experiments/logs/faster_rcnn_end2end_${NET}_${EXTRA_ARGS_SLUG}.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
+LOG="output/faster_rcnn_end2end/${TRAIN_IMDB}/"
 exec &> >(tee -a "$LOG")
 echo Logging output to "$LOG"
 
@@ -57,11 +58,12 @@ time python ./tools/train_net.py --device ${DEV} --device_id ${DEV_ID} \
   ${EXTRA_ARGS}
 
 set +x
-NET_FINAL=`grep -B 1 "done solving" ${LOG} | grep "Wrote snapshot" | awk '{print $4}'`
+NET_FINAL=`grep -A 1 "done solving" ${LOG} | grep "Wrote snapshot" | awk '{print $4}'`
 set -x
+#echo "$NET_FINAL"
 
 time python ./tools/test_net.py --device ${DEV} --device_id ${DEV_ID} \
-  --weights ${NET_FINAL} \
+  --weights ${LOG} \
   --imdb ${TEST_IMDB} \
   --cfg experiments/cfgs/faster_rcnn_end2end.yml \
   --network VGGnet_test \
