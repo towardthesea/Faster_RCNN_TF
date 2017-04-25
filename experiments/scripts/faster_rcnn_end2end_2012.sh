6bd36b8370278fc2ec422db4bec1f0d2f9f307e5
Phuong Nguyen <phuong.nguyen@iit.it>
2017-04-25 11:52:33 +0200

    [scripts] add some scripts for coco and voc12

---
+++ experiments/scripts/faster_rcnn_end2end_2012.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+# Usage:
+# ./experiments/scripts/faster_rcnn_end2end.sh GPU GPU_ID NET DATASET [options args to {train,test}_net.py]
+# DATASET is either pascal_voc or coco.
+#
+# Example:
+# ./experiments/scripts/faster_rcnn_end2end.sh gpu 0 VGG_CNN_M_1024 pascal_voc \
+#   --set EXP_DIR foobar RNG_SEED 42 TRAIN.SCALES "[400, 500, 600, 700]"
+
+set -x
+set -e
+
+export PYTHONUNBUFFERED="True"
+
+DEV=$1
+DEV_ID=$2
+NET=$3
+DATASET=$4
+
+array=( $@ )
+len=${#array[@]}
+EXTRA_ARGS=${array[@]:4:$len}
+EXTRA_ARGS_SLUG=${EXTRA_ARGS// /_}
+
+case $DATASET in
+  pascal_voc)
+    TRAIN_IMDB="voc_2012_trainval"
+    TEST_IMDB="voc_2012_test"
+    PT_DIR="pascal_voc"
+    ITERS=100000
+    ;;
+  coco)
+    # This is a very long and slow training schedule
+    # You can probably use fewer iterations and reduce the
+    # time to the LR drop (set in the solver to 350,000 iterations).
+    TRAIN_IMDB="coco_2014_train"
+    TEST_IMDB="coco_2014_val"
+    PT_DIR="coco"
+    ITERS=490000
+    ;;
+  *)
+    echo "No dataset given"
+    exit
+    ;;
+esac
+
+#LOG="experiments/logs/faster_rcnn_end2end_${NET}_${EXTRA_ARGS_SLUG}.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
+LOG="output/faster_rcnn_end2end/${TRAIN_IMDB}/"
+exec &> >(tee -a "$LOG")
+echo Logging output to "$LOG"
+
+time python ./tools/train_net.py --device ${DEV} --device_id ${DEV_ID} \
+  --weights data/pretrain_model/VGG_imagenet.npy \
+  --imdb ${TRAIN_IMDB} \
+  --iters ${ITERS} \
+  --cfg experiments/cfgs/faster_rcnn_end2end.yml \
+  --network VGGnet_train \
+  ${EXTRA_ARGS}
+
+set +x
+NET_FINAL=`grep -A 1 "done solving" ${LOG} | grep "Wrote snapshot" | awk '{print $4}'`
+set -x
+#echo "$NET_FINAL"
+
+#time python ./tools/test_net.py --device ${DEV} --device_id ${DEV_ID} \
+#  --weights ${LOG} \
+#  --imdb ${TEST_IMDB} \
+#  --cfg experiments/cfgs/faster_rcnn_end2end.yml \
+#  --network VGGnet_test \
+#  ${EXTRA_ARGS}
